0705010ec096cf444af34074fb7ef3eb3594f7b3
Will Coan <wcoan300@gmail.com>
2020-01-29 12:56:46 -0500

    added a loop to add as many members as needed

--- app.js
+++ app.js
@@ -1,79 +1,60 @@
 const inquirer = require("inquirer");
-var { Observable } = require("rxjs");
-
 const managerQs = require('./roles/ManagerQs.js')
 const eQs = require('./roles/EngineerQs.js')
 const iQs = require('./roles/InternQs.js')
 
-let emitter
-
-var prompts = Observable.create(function yes (e) {
-  emitter = e;
-
-  emitter.next({
+function start () {
+  inquirer.prompt(
+    {
     
       type: 'list',
       name: 'member',
       message: 'What employee would you like to add?',
       choices: ['Manager', 'Engineer', 'Intern']
     
-  });
-});
+  }
+  )
+
+  .then(ans => {
+    console.log(ans.member)
+    let answer = ans.member
+    let lowerAns = answer.toLowerCase()
+    setTimeout(eval(lowerAns), 1) 
   
- inquirer.prompt(prompts).ui.process.subscribe(
-  
-    function(ans) {
-      let answer = ans.answer 
-      let lowerAns = answer.toLowerCase()
-      setTimeout(eval(lowerAns), 1) 
-    },
-    function(err) {
-      console.log('Error: ', err);
-    }
-  
-  );
-  
-  
-  
-  function manager () {
-    inquirer.prompt(managerQs).ui.process.subscribe(
-      function(ans) {
-        console.log('Answer is: ', ans);
-      },
-      function(err) {
-        console.log('Error: ', err);
-      },
-      function() {
-  
-      }
-    )
+})
+}
+
+  async function manager () {
+   await inquirer.prompt(managerQs)
+    goAgain()
   }
 
-  function engineer () {
-    inquirer.prompt(eQs).ui.process.subscribe(
-      function(ans) {
-        console.log('Answer is: ', ans);
-      },
-      function(err) {
-        console.log('Error: ', err);
-      },
-      function() {
-  
-      }
-    )
+  async function engineer () {
+    await inquirer.prompt(eQs)
+    goAgain()
   }
 
-  function intern () {
-    inquirer.prompt(iQs).ui.process.subscribe(
-      function(ans) {
-        console.log('Answer is: ', ans);
-      },
-      function(err) {
-        console.log('Error: ', err);
-      },
-      function() {
-    
-      }
-    )
+  async function intern () {
+    await inquirer.prompt(iQs)
+    goAgain()
   }
 
+  function goAgain () {
+    inquirer.prompt(
+      {
+      
+        type: 'confirm',
+        name: 'another',
+        message: 'Would you like to add another Employee?',
+      })
+
+      .then(ans => {
+        console.log(ans)
+        if(ans.another) {
+          start()
+        }
+      
+    })
+  }
+
+  start()
